8c44389aede618c388a301f8656740068ba1ee27
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-06-05 14:57:47 -0600

    Creates App with useEffect loading a dog picture after rendering and storing it in state for display.

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [randomDogImage, setRandomDogImage] = useState("")
+
+  useEffect(() => {fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => response.json())
+    .then((data) => {
+      setRandomDogImage(data.message);
+      console.log(data.message)
+      setIsLoaded(true);
+    });
+  }, [] )
+
+
+   // if the data hasn't been loaded, show a loading indicator
+  if (!isLoaded) return <h3>Loading...</h3>;
+
+  return  <img src= {randomDogImage} alt="A Random Dog"/> ; 
+}
+
+export default App
+  
